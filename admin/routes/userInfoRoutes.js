cb598e1e75ae76719e2eb5ec34bda6a78f594934
amitpulse <32238396+amitpulse@users.noreply.github.com>
2022-10-06 10:35:17 +0530

    models and routes

--- admin/routes/userInfoRoutes.js
+++ admin/routes/userInfoRoutes.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
-const uuid = require('uuid')
-// const UserInfo = require('../models/profileModel')
+
 
 const {createAdditionalInfo} = require('../controllers/userInfoController')
 
-// const storage = multer.diskStorage({
-//     destination:  (req, file, cb) => {
-//       cb(null, '..images')
-//     },
-//     filename: (req, file, cb) => {
-//         const uniqueSuffix = uuid.v4()  + '-' + Date.now();
-//         cb(null, file.originalname + '-' + uniqueSuffix)
-//     }
-// })
-
-// const upload = multer({ imageStorage: storage })
-
-// router.post('/userpic', upload.single('photo'), addProfileImage);
-
-// additional info
-// router.post('/addinfo', upload.single('photo'));
 
 router.post('/addinfo', createAdditionalInfo)
 
